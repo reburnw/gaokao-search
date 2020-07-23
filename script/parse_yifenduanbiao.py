7bf5635b199df3f43e1139a27445c8e7e0f4a618
reburnw <reburnw@gmail.com>
2020-07-24 02:08:41 +0800

    一分段表的处理脚本

---
+++ script/parse_yifenduanbiao.py
@@ -0,0 +1,64 @@
+import json 
+
+def process(lst):
+    score_info = []
+
+    for idx in range(len(lst)):
+        
+        if idx>0:
+            score_info.append({
+                "score":int(lst[idx].split()[0]),
+                "high":int(lst[idx-1].split()[2]),
+                "low":int(lst[idx].split()[2]),
+            })
+        else :
+            score_info.append({
+                "score":int(lst[idx].split()[0]),
+                "high":0,
+                "low":int(lst[idx].split()[2]),
+            })
+        
+        
+    return score_info
+
+def check(x,y):
+    # print(x,y)
+    if (x["high"]<y["low"]) & (x["low"]>y["high"]):
+        return True
+    else :
+        return False
+
+def main():
+    
+    wen_20 = process(open("./gaokao/gaokao/2020_wen").read().strip().split('\n'))
+    li_20 = process(open("./gaokao/gaokao/2020_li").read().strip().split('\n'))
+
+    wen_19 = process(open("./gaokao/gaokao/2019_wen").read().strip().split('\n'))
+    li_19 = process(open("./gaokao/gaokao/2019_li").read().strip().split('\n'))
+    
+    for i in wen_20:
+        i["related_score"] = []
+        for j in wen_19:
+            if(check(i,j)):
+                i["related_score"].append(j["score"])
+
+    for i in li_20:
+        i["related_score"] = []
+        for j in li_19:
+            if(check(i,j)):
+                i["related_score"].append(j["score"])
+
+    f = open("./gaokao/gaokao/wen.json","w")
+    f.write(json.dumps(wen_20,indent=4))
+    f.close()
+
+    f = open("./gaokao/gaokao/li.json","w")
+    f.write(json.dumps(li_20,indent=4))
+    f.close()
+    
+    
+        
+    
+
+if __name__ == "__main__":
+    main()
